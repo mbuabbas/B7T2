da7642d17a9568d3bd682670d5aef194495b042f
Uran Suranchiyev <uransura@gmail.com>
2023-05-09 12:24:32 -0400

    added new AD-22 API story

--- src/test/java/step_definitions/API/Api_StepDefinitions.java
+++ src/test/java/step_definitions/API/Api_StepDefinitions.java
@@ -4,24 +4,15 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import io.restassured.http.ContentType;
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
 import io.restassured.specification.RequestSpecification;
 import org.junit.Assert;
-
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-
-import static io.restassured.RestAssured.given;
 
 
 public class Api_StepDefinitions {
     Response response;
-    HashMap<String, String> actualFields;
-    HashMap<String, String> actualValues;
     private String endpoint;
 
     @Given("the {string} course endpoint is {string}")
@@ -35,33 +26,16 @@ public class Api_StepDefinitions {
         }
     }
 
-    @When("I send a GET request to the SDET course endpoint")
-    public void iSendAGETRequestToTheSDETCourseEndpoint() {
-        RequestSpecification request = RestAssured.given();
-        response = request.get(endpoint).then().log().all().extract().response();
-    }
-
     @Then("the response status code is {int}")
     public void theResponseStatusCodeIs(int code) {
         Assert.assertEquals(code, response.statusCode());
     }
 
-
-    @And("the response body contains the following fields:")
-    public void theResponseBodyContainsTheFollowingFields(List<String> expectedFields) {
-        response = response.then().log().all().extract().response();
-        for (String field : expectedFields) {
-            Assert.assertTrue(response.getBody().asString().contains(field));
-
-        }
-
-    }
-
-    @When("I send a POST request to the SDET course endpoint with body:")
-    public void iSendAPOSTRequestToTheSDETCourseEndpointWithBody(Map<String, String> map) {
+    @When("I send a POST request with body {string}, {string}")
+    public void iSendAPOSTRequestToTheSDETCourseEndpointWithBody(String name, String duration) {
         String payload = "{\n" +
-                "  \"name\": \"Uran test Sasuke\",\n" +
-                "  \"duration\": \"1002 days\"\n" +
+                "  \"name\": \""+ name + "\",\n" +
+                "  \"duration\": \""+ duration + "\"\n" +
                 "}";
         RequestSpecification request = RestAssured.given();
         response = request.header("Content-type", "application/json")
@@ -74,20 +48,26 @@ public class Api_StepDefinitions {
                 .extract().response();
     }
 
-    @When("I send a DELETE request to the SDET course with name {string}")
-    public void iSendADELETERequestToTheSDETCourseWithName(String name) {
+    @And("the response body contains the following fields {string}, {string}")
+    public void theResponseBodyContainsTheFollowingFields(String name, String duration) {
+        response = response.then().log().all().extract().response();
+        Assert.assertTrue(response.getBody().asString().contains(name));
+        Assert.assertTrue(response.getBody().asString().contains(duration));
+    }
+
+    @When("I send a DELETE request to the SDET course with {string}")
+    public void iSendADELETERequestToTheSDETCourseWith(String name) {
         RequestSpecification request = RestAssured.given();
         response = request.header("Content-type", "application/json")
                 .when()
                 .delete(endpoint + "?name=" + name)
                 .then()
-                .statusCode(200)
                 .log().all()
                 .extract().response();
     }
 
-    @And("the name {string} not exist in  response body")
-    public void theNameNotExistInResponseBody(String name) {
+    @And("the {string} not exist in  response body")
+    public void theNotExistInResponseBody(String name) {
         response = response.then().log().all().extract().response();
         if(response.getBody().asString().contains(name)){
             Assert.fail();
