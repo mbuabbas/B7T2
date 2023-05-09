9f7f20134da09abf7e8da15bf7c7c60aa150847d
klvn kl <klvnklvn12@gmail.com>
2023-05-09 19:51:03 -0400

    AD-4

--- src/test/java/runners/CukesRunner.java
+++ src/test/java/runners/CukesRunner.java
@@ -16,7 +16,7 @@ import org.junit.runner.RunWith;
         glue = "step_definitions/UI",
         stepNotifications = true,
         dryRun = false
-        ,tags = "@AD-6"
+        ,tags = "@AD-4"
 )
 public class CukesRunner {
 
